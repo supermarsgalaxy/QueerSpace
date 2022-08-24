bda7e63124f643787654c31d791cf134ed8e59db
Mars Brown <meeshajean33@gmail.com>
2022-08-24 15:49:24 -0400

    redo template

---
+++ index.js
@@ -0,0 +1,7 @@
+//DOM
+// let tabText = document.getElementsByClassName("tabs").innerHTML;
+// let clickedTab = tabText.bold();
+
+// tabText.addEventListener("click", clickedTab);
+const search = document.getElementById("search-input");
+const vendorList = document.getElementById("boxc");
--- script.js
+++
@@ -1,6 +0,0 @@
-function tabAction() {
-  let tabText = document.getElementsByClassName("tabs").innerHTML;
-  let clickedTab = tabText.bold();
-}
-
-tabText.addEventListener("click", tabAction);
